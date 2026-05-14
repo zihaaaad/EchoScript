3475177bc5fe4828a5961c7aedc5c3b1e8f2ab97
Zihad Hasan <zihad.connects@gmail.com>
2026-05-14 20:14:26 +0600

    Fix: Inject namespace into subprojects to fix isar_flutter_libs build failure on AGP 8+

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -15,6 +15,18 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
+subprojects {
+    afterEvaluate {
+        if (project.hasProperty("android")) {
+            val android = project.extensions.getByName("android") as com.android.build.gradle.BaseExtension
+            if (android.namespace == null) {
+                android.namespace = "com.google.dev.echoscript.${project.name.replace("-", "_")}"
+            }
+        }
+    }
+}
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
